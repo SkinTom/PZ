1cf5aa3de6f79828d08d455629bd033b16edacd8
TomaszSkinder <tomaszskinder0@gmail.com>
2020-11-21 18:56:30 +0100

    Separation of the side menu and the top menu to a separate file

--- src/main/java/pz/grupa5a/athena/controller/AdminController.java
+++ src/main/java/pz/grupa5a/athena/controller/AdminController.java
@@ -1,23 +1,30 @@
 package pz.grupa5a.athena.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
     @GetMapping("/finances")
-    public String finances() {
+    public String finances(Model model) {
+        String menuItem = "finances";
+        model.addAttribute("menuItem", menuItem);
         return "finances";
     }
 
     @GetMapping("/employees")
-    public String employees() {
+    public String employees(Model model) {
+        String menuItem = "employees";
+        model.addAttribute("menuItem", menuItem);
         return "employees";
     }
 
     @GetMapping("/items")
-    public String items() {
+    public String items(Model model) {
+        String menuItem = "items";
+        model.addAttribute("menuItem", menuItem);
         return "items";
     }
 }
