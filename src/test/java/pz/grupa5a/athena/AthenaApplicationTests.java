f61755250ff6603bc3b9a53ad382eaea390a23e8
TomaszSkinder <tomaszskinder0@gmail.com>
2020-11-21 12:28:29 +0100

    New Spring Boot project created

---
+++ src/test/java/pz/grupa5a/athena/AthenaApplicationTests.java
@@ -0,0 +1,13 @@
+package pz.grupa5a.athena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
